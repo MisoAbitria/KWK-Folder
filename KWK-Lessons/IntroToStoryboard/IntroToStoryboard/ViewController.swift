b1cacd45c74a6b387d82b1ecf001dcf9b573420e
MisoAbitria <87437023+MisoAbitria@users.noreply.github.com>
2021-07-14 11:37:31 -0400

    1st Version

---
+++ KWK-Lessons/IntroToStoryboard/IntroToStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroToStoryboard
+//
+//  Created by  Scholar on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statment - testing testing!")
+    }
+
+
+}
+
