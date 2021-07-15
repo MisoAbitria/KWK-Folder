1f49658c118b8d39ce244a015e43f845c1078278
MisoAbitria <87437023+MisoAbitria@users.noreply.github.com>
2021-07-14 20:38:59 -0400

    Mini proj (1) V2

--- MiniProject/MiniProject/MiniProject/ViewController.swift
+++ MiniProject/MiniProject/MiniProject/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    var f1 = "- Fact 1"
+    var f2 = "- Fact 2"
+    var f3 = "- Fact 3"
+    
+    @IBOutlet weak var inF1: UILabel!
+    @IBOutlet weak var inF2: UILabel!
+    @IBOutlet weak var inF3: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 //        print("This is a print statment - testing testing!")
+        showText()
+    }
+    func showText(){
+    inF1.text = f1
+    inF2.text = f2
+    inF3.text = f3
     }
 
+    @IBAction func Click(_ sender: UIButton) {
+        f1 = "- I mostly work alone."
+        f2 = "- I use to play 3 instruments"
+        f3 = "- I am an introvert."
+        showText()
+    }
     
 }
 
