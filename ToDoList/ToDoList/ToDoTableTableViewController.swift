e4c5ee4bbd0fe5bb0606c188b8a8251c3d00e80c
MisoAbitria <87437023+MisoAbitria@users.noreply.github.com>
2021-07-19 12:11:59 -0400

    Add files via upload
    Complete Iteration 1

--- ToDoList/ToDoList/ToDoTableTableViewController.swift
+++ ToDoList/ToDoList/ToDoTableTableViewController.swift
@@ -39,10 +39,16 @@ class ToDoTableTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let eachToDo = listOfToDo[indexPath.row]
+        
+        performSegue(withIdentifier: "moveToCompletedToDoVC", sender: eachToDo)
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 2
+        return listOfToDo.count
     }
 
     
@@ -66,14 +72,24 @@ class ToDoTableTableViewController: UITableViewController {
 
 
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let nextAddToDoVC = segue.destination as? AddToDoViewController{
+            nextAddToDoVC.previousToDoTVC = self
+        }
+        if let nextCompletedToDoVC = segue.destination as? CompletedToDoViewController {
+            if let choosenToDo = sender as? ToDoClass {
+                nextCompletedToDoVC.selectedToDo = choosenToDo
+                nextCompletedToDoVC.previousToDoTVC = self
+            }
+        }
+        
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
